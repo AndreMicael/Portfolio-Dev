f9ccd0b2822bc0eb41391e3152a8fa2d7d0cce2b
unknown <andremicael@gmail.com>
2023-10-27 13:53:29 -0400

    Projetos atualizado

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import Mockup from '../assets/Mockup';
 import Thumb from '../img/telas.png';
 import Projeto01 from "../img/projeto01.png"
+import Projeto02 from "../img/projeto02.png"
 
 function Projects() {
     return (  
@@ -14,7 +15,7 @@ function Projects() {
             <h1> Projetos.</h1>
           
             <Mockup title="Ion Imoveis"
-            description=" Mussum Ipsum, cacilds vidis litro abertis. Interagi no mé, cursus quis, vehicula ac nisi."
+            description="Landing page sobre um novo lançamento imobiliário criado para prospectar clientes para a imobiliária."
             stacks="HTML | CSS | Javascript"
             gitlink='https://github.com/AndreMicael/Ion-Imoveis-LandingPage'
             sitelink='https://andremicael.github.io/Ion-Imoveis-LandingPage/'
@@ -23,13 +24,13 @@ function Projects() {
             position=''             
             ></Mockup>
             <div className='d-none d-sm-block'>
-                 <Mockup title="Projeto 02"
-            description=" Mussum Ipsum, cacilds vidis litro abertis. Interagi no mé, cursus quis, vehicula ac nisi."
-            stacks="HTML | CSS | Javascript"
-            gitlink='https://github.com/AndreMicael/Ion-Imoveis-LandingPage'
-            sitelink='https://andremicael.github.io/Ion-Imoveis-LandingPage/'
+                 <Mockup title="Finans - Finanças Pessoais"
+            description="Template criado para reproduzir a homepage de um site voltado a finanças pessoais."
+            stacks="HTML | CSS"
+            gitlink='https://github.com/AndreMicael/Finans-page'
+            sitelink='https://andremicael.github.io/Finans-page/'
             thumb={Thumb}
-            projeto={Projeto01}
+            projeto={Projeto02}
             position='rows'             
             ></Mockup>   </div>         
           
